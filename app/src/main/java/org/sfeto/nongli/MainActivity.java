669c9704779a5e957608dc749f94b301f0970f49
qinghao <qingxianhao@gmail.com>
2016-11-25 12:09:30 +0800

    listen to screen on/off

--- app/src/main/java/org/sfeto/nongli/MainActivity.java
+++ app/src/main/java/org/sfeto/nongli/MainActivity.java
@@ -1,18 +1,26 @@
 package org.sfeto.nongli;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
 
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                registerBroadcastReceiver();
+            }
+        }).run();
+
         String last_class = MyService.readConfig(this);
         String className = last_class.substring(last_class.lastIndexOf(".")+1).toLowerCase();
         int res_id;
@@ -24,7 +32,6 @@ public class MainActivity extends Activity {
         }
 
         res_id = getResources().getIdentifier(className, "drawable", getPackageName());
-
         ImageView img_nongli = (ImageView) findViewById(R.id.img_nongli);
         img_nongli.setImageResource(res_id);
     }
@@ -35,6 +42,21 @@ public class MainActivity extends Activity {
         Intent intent = new Intent(this, MyService.class);
         startService(intent);
         finish();
+    }
+
+    private void registerBroadcastReceiver()
+    {
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_SCREEN_ON);
+        intentFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        intentFilter.addAction("com.android.calendar.date_changed");
+        intentFilter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
+        intentFilter.addAction(Intent.ACTION_TIME_CHANGED);
+        intentFilter.addAction(Intent.ACTION_LOCALE_CHANGED);
+        intentFilter.addAction(Intent.ACTION_DATE_CHANGED);
+        Context context = getApplicationContext();
+        DateChangedReceiver receiver = new DateChangedReceiver();
+        context.registerReceiver(receiver, intentFilter);
     }
 
 
